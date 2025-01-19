c3824cae244b80ceb8613057085a1e100ab72c84
AGUNG NUGRAHA <232220013@unmuhpnk.ac.id>
2025-01-19 21:30:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
